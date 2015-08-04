657145d97afe2d82dfa705f6ac7077bf160fd8ea
Prince Annan Koomson <annan.pk@gmail.com>
2015-08-04 09:06:01 +0000

    DB

---
+++ Adpc/Data Classes/ClassGlobalMethods.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Adpc.Data_Classes
+{
+    class ClassGlobalMethods
+    {
+
+    }
+}
--- Adpc/Database/Database.sql
+++ Adpc/Database/Database.sql
@@ -1,3 +1,14 @@
 ﻿Create Database `adpc`;
 
 use `adpc`;
+
+CREATE TABLE `airlines`(
+  `airline_id` int(5) unsigned NOT NULL AUTO_INCREMENT,
+  `airline_name` varchar(50) DEFAULT NULL,
+  `created_date` datetime DEFAULT cur_date(),
+  `airline_code` char(50) DEFAULT NULL,
+  `active` tinyint(1),
+  PRIMARY KEY `airline_id_key` (`airline_id`) ,
+  UNIQUE KEY `airline_id_code` (`airline_code`),
+  );
+
