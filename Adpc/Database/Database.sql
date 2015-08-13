f43b1905cf1337b9a3f421b200ff3d52d88235e1
cydanq <kwekudanquah@gmail.com>
2015-08-13 08:40:08 +0100

    updated codes

--- Adpc/Database/Database.sql
+++ Adpc/Database/Database.sql
@@ -36,3 +36,54 @@ CREATE TABLE `airport_cities`(
   UNIQUE KEY `city_id_code` (`city_code`),
   CONSTRAINT `airport_cities_country_idfk` FOREIGN KEY (`country_id`) REFERENCES `Countries` (`id`)
   );
+
+  /* [KOD] */
+  CREATE TABLE `users`(
+  `user_id` int(5) unsigned NOT NULL AUTO_INCREMENT,
+  `full_name` varchar(50) DEFAULT NULL,
+  `login_name` varchar(50) DEFAULT NULL,
+  `password` char(50) DEFAULT NULL,
+  `phone` char(30) DEFAULT NULL,
+  `e_mail` char(50) DEFAULT NULL,
+  `address` char(50) DEFAULT NULL,
+  `active` tinyint(1) DEFAULT 1,
+  `last_update_date` datetime DEFAULT  NOW(),
+  PRIMARY KEY `user_id_key` (`user_id`) ,
+  UNIQUE KEY `login_name` (`login_name`),
+  );
+
+  /* [KOD] */
+  CREATE TABLE `user_permissions` (
+  `permission_id` int(5) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(5) unsigned NOT NULL,
+  `user_role` varchar(30) DEFAULT NULL,
+  `description` char(50) DEFAULT NULL,
+  `active` tinyint(1) DEFAULT 1,
+  PRIMARY KEY `permission_id_key`(`permission_id`),
+  UNIQUE KEY `user_role` (`user_role`),
+  CONSTRAINT `permision_id_user_idfk` FOREIGN KEY (`user_id`),
+  );
+
+  /* [KOD] */
+  CREATE TABLE `declaration` (
+  `declaration_id` int(5) unsigned NOT NULL AUTO_INCREMENT,
+  `doc_number` int(5) unsigned NOT NULL AUTO_INCREMENT,
+  `doc_id` char(30) DEFAULT NULL,
+  `doc_date` datetime DEFAULT  NOW(),
+  `consignee` char(30) DEFAULT NULL,
+  `consignee_address` char(50) DEFAULT NULL,
+  PRIMARY KEY `declaration_id_key` (`declaration_id`),
+  UNIQUE KEY `doc_number` (`doc_number`),
+  CONSTRAINT `declaration_id_doc_idfk` FOREIGN KEY (`doc_id`),
+
+  /* [KOD] */
+  CREATE TABLE `doc_items` (
+  `doc_id` int(5) unsigned NOT NULL AUTO_INCREMENT,
+  `declaration_id` int(5) unsigned NOT NULL AUTO_INCREMENT,
+  `item_description` char(50) DEFAULT NULL,
+  `package` char(30) DEFAULT NULL,
+  `gross_weight` int(10) unsigned NOT NULL,
+  `unit_of_measure` char(20) DEFAULT NULL,
+  PRIMARY KEY `doc_id_key` (`doc_id`),
+  UNIQUE KEY
+  CONSTRAINT `doc_id_declaration_idfk` FOREIGN KEY (`declaration_id`),
